639d98035e821e9ff525593050bb3ba7ba1dfc08
teruo9589 <teruo9589@gmail.com>
2022-01-23 16:24:31 +0900

    first commit

---
+++ src/main/java/com/example/t_kakeibo/TKakeiboApplication.java
@@ -0,0 +1,13 @@
+package com.example.t_kakeibo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TKakeiboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TKakeiboApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/t_kakeibo/TKakeiboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.t_kakeibo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TKakeiboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
